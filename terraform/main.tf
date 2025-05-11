244fe0b9f261af70d5e70d63ecdbffe32319004f
Liron Binyamin <93601057+LironBinyamin96@users.noreply.github.com>
2025-05-11 12:20:00 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "il-central-1"
 }
 
+# IAM Role for Lambda
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_liron_role"
 
@@ -22,6 +23,7 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+# Lambda Function
 resource "aws_lambda_function" "lambda" {
   function_name = "liron-lambda-auto-terraform"
   role          = aws_iam_role.lambda_exec_role.arn
@@ -31,21 +33,25 @@ resource "aws_lambda_function" "lambda" {
   source_code_hash = filebase64sha256("function.zip")
 }
 
+# Existing API Gateway
 data "aws_api_gateway_rest_api" "existing_api" {
- name = "imtech"
+  name = "imtech"
 }
 
+# Root Resource
 data "aws_api_gateway_resource" "root" {
   rest_api_id = data.aws_api_gateway_rest_api.existing_api.id
   path        = "/"
 }
 
+# New Resource under root
 resource "aws_api_gateway_resource" "lambda_resource" {
   rest_api_id = data.aws_api_gateway_rest_api.existing_api.id
   parent_id   = data.aws_api_gateway_resource.root.id
   path_part   = "liron-lambda-auto-terraform"
 }
 
+# Method on new resource
 resource "aws_api_gateway_method" "post_lambda" {
   rest_api_id   = data.aws_api_gateway_rest_api.existing_api.id
   resource_id   = aws_api_gateway_resource.lambda_resource.id
@@ -53,6 +59,7 @@ resource "aws_api_gateway_method" "post_lambda" {
   authorization = "NONE"
 }
 
+# Integration with Lambda
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = data.aws_api_gateway_rest_api.existing_api.id
   resource_id             = aws_api_gateway_resource.lambda_resource.id
@@ -62,10 +69,39 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   uri                     = aws_lambda_function.lambda.invoke_arn
 }
 
+# Lambda permission for API Gateway
 resource "aws_lambda_permission" "allow_api_gateway" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${data.aws_api_gateway_rest_api.existing_api.execution_arn}/*/POST/lambda"
+  source_arn    = "${data.aws_api_gateway_rest_api.existing_api.execution_arn}/*/ANY/liron-lambda-auto-terraform"
+}
+
+# Deployment
+resource "aws_api_gateway_deployment" "lambda_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration
+  ]
+
+  rest_api_id = data.aws_api_gateway_rest_api.existing_api.id
+
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.lambda_resource.id,
+      aws_api_gateway_method.post_lambda.id,
+      aws_api_gateway_integration.lambda_integration.id,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Stage
+resource "aws_api_gateway_stage" "lambda_stage" {
+  stage_name    = "prod"
+  rest_api_id   = data.aws_api_gateway_rest_api.existing_api.id
+  deployment_id = aws_api_gateway_deployment.lambda_deployment.id
 }
